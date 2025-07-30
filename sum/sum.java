965438bfa593b0490c08e39fbde36e1f44959ae5
Your Name <ak.tech106@gmail.com>
2025-07-30 23:12:30 +0530

    program in sum.java

---
+++ sum/sum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Solution {
+    static boolean isEven(int n) {
+        if (n % 2 == 0) {
+            System.out.println("true");
+            return true;
+        } else {
+            System.out.println("false");
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);  // Create Scanner object for input
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();                 // Read input number
+        isEven(n);                            // Call the method
+        sc.close();                           // Close the scanner
+    }
+}
